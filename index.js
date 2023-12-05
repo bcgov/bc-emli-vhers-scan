734341e354fcb8358230fbdf5aa7e6295d6da0f0
hannah-macdonald1 <hamacdonald@deloitte.ca>
2023-12-05 14:05:37 -0700

    added virus scan logging and db connection

--- index.js
+++ index.js
@@ -8,8 +8,62 @@ const fileUpload = require('express-fileupload');
 const config = require('./config')
 const port = process.env.PORT && process.env.PORT !== '' ? parseInt(process.env.PORT) : 3500;
 
+const startTime = (req, res, next) => {
+    const startTime = process.hrtime();
+    res.set(
+        'X-Start-Time',
+        startTime[0].toString() + ',' + startTime[1].toString(),
+    );
+    next();
+}
+
+const responseLogger = async function (
+    req,
+    res,
+    next,
+) {
+    const originalSendFunc = res.send.bind(res);
+    res.send = function (body) {
+        const startTimeString = res.get('X-Start-Time')?.split(',');
+        const startTime = [
+            Number(startTimeString[0]),
+            Number(startTimeString[1]),
+        ];
+        const diff = process.hrtime(startTime);
+        const time = diff[0] * 1e3 + diff[1] * 1e-6;
+        if (res.get('X-Start-Time')) {
+            res.removeHeader('X-Start-Time');
+        }
+		let reqBody = {}
+		if (req.files && req.files.energuide && req.files.energuide.name) reqBody =  {filename: req.files.energuide.name};
+		createAuditLog(
+			res.statusCode,
+			Number(time.toFixed(3)),
+			reqBody,
+			body,
+		).then(() => {});
+        return originalSendFunc(body);
+    };
+    next();
+}
+
+app.use(startTime);
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(responseLogger)
+
+// DB configuration
+const pg = require('knex')({
+	client: 'pg',
+	connection: {
+	  host: process.env.DB_HOST,
+	  port: process.env.DB_PORT,
+	  user: process.env.DB_USERNAME,
+	  database: process.env.DB_NAME,
+	  password: process.env.DB_PASSWORD,
+	},
+	pool: {min: 0, max: parseInt(process.env.DB_MAX_POOL)}
+});
 
 // Middleware for attaching clamscan with the express request
 app.use(async (req, _, next) => {
@@ -20,6 +74,23 @@ app.use(async (req, _, next) => {
 // Middleware for attaching files to req.files
 app.use(fileUpload({ ...config.fileUploadConfig }))
 
+const createAuditLog = async (statusCode, responseTimeMs, requestBody, responseBody) => {
+	const entry = {
+		endpoint_name: 'POST /virus-scan',
+		status_code: Number(statusCode),
+		response_time_ms: responseTimeMs,
+	}
+	if (requestBody) entry.request_body = requestBody;
+	if (responseBody) entry.response_body = responseBody;
+	try {
+		await pg.insert(
+			entry
+		).into('vhers_audit_log'); 
+	}
+	catch (err) {
+		console.log(err);
+	}
+}
 
 const scanFile = async (file, clamscan) => {
 	const fileStream = Readable()
@@ -34,7 +105,7 @@ const scanFile = async (file, clamscan) => {
 	  is_infected: result.isInfected,
 	  viruses: result.viruses,
 	}
-  }
+}
 
 app.post('/virus-scan', async (req, res) => {
 	if (!req.files || ! req.files.energuide) {
@@ -43,7 +114,15 @@ app.post('/virus-scan', async (req, res) => {
 		})
 	}
 	// await req.files.energuide.mv('./tmp/' + req.files.energuide.name);
-  const scanResult = await scanFile(req.files.energuide, req.clamscan);
+  let scanResult; 
+  try {
+  	scanResult = await scanFile(req.files.energuide, req.clamscan);
+  } catch (err) {
+	console.log(err);
+	return res.status(500).json({
+		message: "Scan failed due to internal server error"
+	})
+  }
   console.log(scanResult);
 
   if (scanResult.is_infected === true || scanFile.is_infected === null) {
